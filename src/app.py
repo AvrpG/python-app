644a49db0518c7565ca49f0532a0f6abb6d54d3f
AvrpG <AvrpG@example.com>
2026-03-01 19:05:22 +0530

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import socket
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+@app.route('/api/v1/details')
+def details():
+    return jsonify({"time": datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
+                    "message": "This is the details endpoint", "host": socket.gethostname()}), 200
+
+@app.route('/api/v1/health')
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
